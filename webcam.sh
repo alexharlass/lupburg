d7efb6c43a7ff9971ad179f6aa1f01d09394a706
AlexHarlass <mail@alexander-harlass.de>
2014-12-29 17:33:59 +0100

    generated files on 2014-12-29 17:33:59

--- webcam.sh
+++ webcam.sh
@@ -3,7 +3,7 @@
 fswebcam -r 960x720 -d /dev/video0 /home/pi/webcam.jpg
 cp /home/pi/webcam.jpg /home/pi/lupburg/
 cd /home/pi/lupburg/
-sleep 7
+sleep 10
 git add .
 git commit -a -m "generated files on `date +'%Y-%m-%d %H:%M:%S'`"
 git push -u origin master
