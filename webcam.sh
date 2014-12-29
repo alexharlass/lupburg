e22f7c7e3bbefe526033e31f768e5b469642dfda
AlexHarlass <mail@alexander-harlass.de>
2014-12-29 16:15:27 +0100

    1419866127

--- webcam.sh
+++ webcam.sh
@@ -3,6 +3,7 @@
 fswebcam -r 960x720 -d /dev/video0 /home/pi/webcam.jpg
 cp /home/pi/webcam.jpg /home/pi/lupburg/
 cd /home/pi/lupburg/
+git add .
 git commit -a -m `date +%s`
 git push -u origin master
 
