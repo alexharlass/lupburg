4ec955942916cf4dcb81e33c2c03a32c80afd016
AlexHarlass <mail@alexander-harlass.de>
2014-12-29 16:23:03 +0100

    date +%s

--- webcam.sh
+++ webcam.sh
@@ -4,6 +4,6 @@ fswebcam -r 960x720 -d /dev/video0 /home/pi/webcam.jpg
 cp /home/pi/webcam.jpg /home/pi/lupburg/
 cd /home/pi/lupburg/
 git add .
-git commit -a -m "test"
+git commit -a -m 'date +%s'
 git push -u origin master
 
